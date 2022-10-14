508d64fa674b5726b4cba1817b3c2bd29a88392f
Sam Selikoff <sam.selikoff@gmail.com>
2022-10-14 16:32:47 -0400

    wip

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,41 @@
 import "tailwindcss/tailwind.css";
+import { experimental_use as use } from "react";
+import Link from "next/link";
+import MovieLink from "./movie-link";
+
+async function getMovies() {
+  let res = await fetch("http://localhost:3001/movies");
+
+  return res.json();
+}
 
 export default function RootLayout({ children }) {
+  let movies = use(getMovies());
+
   return (
     <html lang="en" className="text-gray-900">
       <head>
         <title>Next.js</title>
       </head>
 
-      <body>{children}</body>
+      <body>
+        <div className="flex">
+          <nav className="p-4 border-r w-1/3">
+            <p className="text-2xl">
+              <Link href="/movies">Movies</Link>
+            </p>
+            <div className="mt-6">
+              {movies.map((movie) => (
+                <div key={movie.id}>
+                  <MovieLink movieId={movie.id}>{movie.title}</MovieLink>
+                </div>
+              ))}
+            </div>
+          </nav>
+
+          <main className="w-2/3 p-8">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 export default function Page() {
   return (
     <div>
-      <p className="text-lg">Welcome!</p>
-      <p>
-        <Link href="/movies/1">movies/1</Link>
-        <Link href="/movies/2">movies/2</Link>
-      </p>
+      <p className="text-lg">Choose a movie.</p>
     </div>
   );
 }
