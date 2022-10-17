4831f565035527f2ad0819a034575ec0a51b34ed
Sam Selikoff <sam.selikoff@gmail.com>
2022-10-17 14:15:02 -0400

    begin

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,11 @@
-import "tailwindcss/tailwind.css";
-import NavLink from "./nav-link";
-
 export default function RootLayout({ children }) {
   return (
-    <html className="antialiased bg-zinc-900 text-zinc-200">
+    <html>
       <head>
         <title>Nested layouts by example</title>
       </head>
 
-      <body>
-        <header className="border-b border-gray-700 p-4 space-x-4">
-          <NavLink className="underline" href="/">
-            Home
-          </NavLink>
-          <NavLink href="/movies">Movies</NavLink>
-        </header>
-
-        <main className="p-4">{children}</main>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
