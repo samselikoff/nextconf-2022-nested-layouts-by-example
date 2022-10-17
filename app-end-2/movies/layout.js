243455333c82b23bab07446f0a96d034e001c0fd
Sam Selikoff <sam.selikoff@gmail.com>
2022-10-17 13:32:29 -0400

    end

---
+++ app-end-2/movies/layout.js
@@ -0,0 +1,36 @@
+import { experimental_use as use } from "react";
+import MovieLink from "./movie-link";
+
+async function getMovies() {
+  console.log("fetching movies: ", new Date().toISOString().slice(11, 22));
+  let res = await fetch("http://localhost:3001/movies");
+
+  return res.json();
+}
+
+export default function Layout({ children }) {
+  let movies = use(getMovies());
+
+  return (
+    <div className="flex">
+      <ul className="pr-10 text-sm text-gray-400 space-y-1 flex-none">
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <MovieLink href={`/movies/${movie.id}`}>{movie.title}</MovieLink>
+          </li>
+        ))}
+      </ul>
+
+      <div>{children}</div>
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  let res = await fetch("http://localhost:3001/movies");
+  let movies = await res.json();
+
+  return movies.map((movie) => ({
+    id: movie.id,
+  }));
+}
